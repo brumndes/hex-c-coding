768f12303c780241a8c964a0d2e8a2dbb71fd9c2
brumndes <bruno.mds010@outlook.com>
2021-04-12 19:58:33 +0000

    fixing

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,23 @@
 /*
- * File: 2-print_alphabet.c
+ * File: 3-print_alphabets.c
  * Author: Bruno Mendes
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - It gets the last number of a random number and outputs some infos
+ * main - prints the alphabet in lowecase and highercase
  * Return: it  returns 0
  */
 int main(void)
 {
-	char b, B;
+	char b,B;
 
 	for (b = 'a'; b <= 'z'; b++)
 	{
 		putchar(b);
 	}
-	 for (B = 'A'; B <= 'Z'; B++)
+	for (B = 'A'; B <= 'Z'; B++)
         {
                 putchar(B);
         }
