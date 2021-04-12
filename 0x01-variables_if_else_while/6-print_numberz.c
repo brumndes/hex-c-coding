711d462e378552a2c8765407704489998b387bf8
brumndes <bruno.mds010@outlook.com>
2021-04-12 21:43:50 +0000

    fixing

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,12 +11,12 @@
  */
 int main(void)
 {
-	int b = 0;
+	char b;
 
-	while (b < 10)
+	for (b = '0'; b <= '9'; b++)
 	{
-		b = b + 1;
-		printf("%d", b);
+		putchar(b);
 	}
+	putchar('\n');
 	return (0);
 }
