879a01a7dbeb52d3b8bc38dce4e6842d73f39752
brumndes <bruno.mds010@outlook.com>
2021-04-12 19:25:31 +0000

    tried at least

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+/*
+ * File: 2-print_alphabet.c
+ * Author: Bruno Mendes
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - It gets the last number of a random number and outputs some infos
+ * Return: it  returns 0
+ */
+int main(void)
+[
+	char[50] = abcdefghijklmnopqrstuvwxyz;
+	printf("%s", char[50]);
+	return (0);
+}
