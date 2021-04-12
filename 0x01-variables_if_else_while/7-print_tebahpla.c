5253c28f52b429236aaa5a22d70b68389b1eb896
brumndes <bruno.mds010@outlook.com>
2021-04-12 21:49:36 +0000

    changing

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+/*
+ * File: 7-print_tebahpla.c
+ * Author: Bruno Mendes
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - It gets the last number of a random number and outputs some infos
+ * Return: it  returns 0
+ */
+int main(void)
+{
+	char b;
+
+	for (b = 'z'; b <= 'a'; b--)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
