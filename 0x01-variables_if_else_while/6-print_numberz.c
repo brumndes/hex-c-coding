fb78dbf4255bf269308aee3e358c23e3f2579968
brumndes <bruno.mds010@outlook.com>
2021-04-12 21:41:57 +0000

    fixing

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-print_alphabet.c
+ * Author: Bruno Mendes
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - It gets the last number of a random number and outputs some infos
+ * Return: it  returns 0
+ */
+int main(void)
+{
+	int b = 0;
+
+	while (b < 10)
+	{
+		b = b + 1;
+		printf("%d", b);
+	}
+	return (0);
+}
