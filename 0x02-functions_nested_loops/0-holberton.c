a2bc02a0a69bcf792af4b9c3a347b4486845ec9d
brumndes <bruno.mds010@outlook.com>
2021-04-13 21:17:17 +0000

    coding

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -10,6 +10,15 @@
  */
 int main(void)
 {
-	printf("Holberton/n")
+	_putchar(72);
+	_putchar(111);
+	_putchar(108);
+	_putchar(98);
+	_putchar(101);
+	_putchar(114);
+	_putchar(116);
+	_putchar(111);
+	_putchar(110);
+	_putchar(10);
 	return (0);
 }
