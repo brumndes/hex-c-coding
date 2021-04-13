42b1e8a6b4202e6960532a704e640ff959aa8bcb
brumndes <bruno.mds010@outlook.com>
2021-04-13 21:06:05 +0000

    0_holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+/*
+ * File: 0
+ * Author: Bruno Mendes
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints Holberton
+ * Return: it  returns 0
+ */
+int main(void)
+{
+	printf("Holberton/n")
+	return (0);
+}
