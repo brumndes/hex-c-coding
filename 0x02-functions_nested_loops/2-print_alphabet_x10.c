1492e14b431e09f811f231aeaf6a83dd1572acb5
brumndes <bruno.mds010@outlook.com>
2021-04-15 18:23:41 +0000

    just commiting

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+/*
+ * File: 0
+ * Author: Bruno Mendes
+ */
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return: it  returns 0
+ */
+void print_alphabet_x10(void)
+{
+	char b;
+	int x = 0;
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		while x < 10
+		{
+			x++;
+			_putchar(b);
+		}	
+	}
+	_putchar('\n');
+}
