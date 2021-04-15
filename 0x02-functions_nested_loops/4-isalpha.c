04f4d2d42e6bdd99b50fe8128242302e2711ef7f
brumndes <bruno.mds010@outlook.com>
2021-04-15 20:46:09 +0000

    trying

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+/*
+ * File: 0
+ * Author: Bruno Mendes
+ */
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *_islower  - tests if the word is lowercase
+ * Return: it  returns 0
+ */
+int _islower(int c)
+{
+	if ((c > 96) && (c < 123))
+	{
+		return (1);
+	}
+	else if ((c > 96) && (c < 123))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
