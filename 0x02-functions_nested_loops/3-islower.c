18242286e41c174ec1dfcd752e138bcb49b36a98
brumndes <bruno.mds010@outlook.com>
2021-04-15 19:34:39 +0000

    fixing

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+/*
+ * File: 0
+ * Author: Bruno Mendes
+ */
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *  - prints alphabet
+ * Return: it  returns 0
+ */
+int _islower(int c)
+{
+	if (c == true)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
