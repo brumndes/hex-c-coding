e9a243e32d62c92a90be0220468e3ebb16ec519e
brumndes <bruno.mds010@outlook.com>
2021-04-19 21:17:23 +0000

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,9 +4,6 @@
  */
 #include <stdio.h>
 #include "holberton.h"
-/**
- * jack_bauer - a function prints every minute of the day from 00:00 to 23_59 
- */
 void hr_1(void);
 void hr_2(void);
 void jack_bauer(void)
@@ -16,7 +13,7 @@ void jack_bauer(void)
 }
 /**
  * hr_1 - returns each minute for the first 20hrs
- *
+ * jack_bauer - a function prints every minute of the day from 00:00 to 23_59 
  * @hr_1 -  digit
  */
 void hr_1(void)
